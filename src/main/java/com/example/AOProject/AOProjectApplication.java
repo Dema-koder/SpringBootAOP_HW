aaea52378f27acc512e165fdf5274e8eacebc857
Dema-koder <asotruzikovic@gmail.com>
2024-08-11 06:35:56 +0300

    Initial commit

---
+++ src/main/java/com/example/AOProject/AOProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.AOProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AOProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AOProjectApplication.class, args);
+	}
+
+}
